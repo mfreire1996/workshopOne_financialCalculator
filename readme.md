dd6da2a4cfc678323d708bbe41f8a4a69d2164ec
Marcos Freire <mfreire@yearup.appdev.org>
2025-04-13 12:01:43 -0400

    Added description and screenshots in README

--- readme.md
+++ readme.md
@@ -1,1 +1,29 @@
 # Financial Calculator
+
+# Mortgage Calculator
+
+So in the mortgage calculator we used the formula: 
+M=P*(i*(1+i)^n / ((1+i) ^n)-1)
+
+To find out how much their loan would make with 
+a certain amount of interest and time 
+would make over the years.
+
+Users are able to insert their info thanks to the use of scanners. We also took the time to write the formula thanks 
+to the help of Math.pow()
+![img_3.png](img_3.png)
+![img_4.png](img_4.png)
+
+# Future Value Calculator
+
+For future value we used:
+FV = P × (1 + (r / 365))^(365 × t)
+
+To determine how much money is earned within a certain amount of time. 
+
+What I found interesting about both calculators is how java 
+can be used for everyday stuff and stuffs you don't really think about coding. 
+If you can't find something you're looking for then it's best to do it yourself 
+thanks to the help of java. 
+![img_2.png](img_2.png)
+![img_1.png](img_1.png)
--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -5,34 +5,60 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //Calculator 2
-        System.out.print("Enter principal: ");
-        double principal2 = scanner.nextDouble();
+//        //Calculator 2
+//        System.out.print("Enter principal: ");
+//        double principal2 = scanner.nextDouble();
+//        if (principal2 <= 0){
+//            System.out.println("Principle must be greater than 0");
+//            System.exit(0);
+//    }
+//
+//        System.out.print("Enter interest rate: ");
+//        double interestRate = scanner.nextDouble();
+//        if (interestRate <= 0){
+//            System.out.println("Interest must be greater than 0");
+//            System.exit(0);
+//        }
+//
+//        System.out.print("Enter the amount of time: ");
+//        int amountOfYears = scanner.nextInt();
+//        if (amountOfYears <= 0){
+//            System.out.println("Years must be greater than 0");
+//            System.exit(0);
+//        }
+//
+//        double annualInterestRate = interestRate / 100;
+//        double futureValueFormula = principal2 * Math.pow((1 + annualInterestRate / 365), 365 * amountOfYears);
+//
+//        double interestEarned = futureValueFormula - principal2;
+//
+//        System.out.printf("Your future value: %.2f", futureValueFormula);
+//        System.out.printf("\nInterest earned: %.2f", interestEarned);
 
-        System.out.print("Enter interest rate: ");
-        double interestRate = scanner.nextDouble();
-
-        System.out.print("Enter the amount of time: ");
-        int amountOfYears = scanner.nextInt();
-
-        double annualInterestRate = interestRate / 100;
-        double futureValueFormula = principal2 * Math.pow((1 + annualInterestRate / 365), 365 * amountOfYears);
-
-        double interestEarned = futureValueFormula - principal2;
-
-        System.out.printf("Your future value: %.2f", futureValueFormula);
-        System.out.printf("\nInterest earned: %.2f", interestEarned);
+//        End of Calc 2
 
         //Calculator 1
 
 //        System.out.print("1. Enter principal: ");
 //        double principal = scanner.nextDouble();
+//        if (principal <= 0) {
+//            System.out.println("Principle must be greater than 0");
+//            System.exit(0);
+//        }
 //
 //        System.out.print("2. Enter interest rate: ");
 //        double interest = scanner.nextDouble();
+//        if (interest <= 0) {
+//            System.out.println("Interest must be greater than 0");
+//            System.exit(0);
+//        }
 //
 //        System.out.print("3. Enter loan length: ");
 //        double loan = scanner.nextDouble();
+//        if (loan <= 0){
+//            System.out.println("Loan must be greater than 0");
+//            System.exit(0);
+//        }
 //
 //
 //        double monthlyInterestRate = interest / 12 / 100;
@@ -43,5 +69,7 @@ public class Main {
 //        System.out.printf("Monthly payment: %.2f", formula);
 //        System.out.printf("\nInterest payment: %.2f", totalInterest);
 
+//        End of Calc 1
+
+        }
     }
-}
